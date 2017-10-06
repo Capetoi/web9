c600b040a6f83215a74d10b6f3af69b710e0e2d4
capetoi1231997 <capetoi1231997@gmail.com>
2017-10-07 00:19:36 +0700

    update2(10/7/2017)

--- practice/Practice/Practice/Practice3/generatePractice.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-function generate(numberOfTestcases, filePath = "./test-data.json"){
-  return Array.from(Array(numberOfTestcases)); // Remove this line and change to your own algorithm
-}
-
-module.exports = generate
